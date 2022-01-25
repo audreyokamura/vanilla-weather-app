72712553d364cd6fea49fa33cc972364cf7aaa58
audreyokamura <audreyqc@gmail.com>
2022-01-25 08:13:47 +0000

    Created js to display current data

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
-function showTemperature(response) {
-  console.log(response.data);
+function displayCurrentData(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#main-temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#max-temp").innerHTML = Math.round(
+    response.data.main.temp_max
+  );
+  document.querySelector("#min-temp").innerHTML = Math.round(
+    response.data.main.temp_min
+  );
 }
 
 let apiKey = "47b6364afb2ed8bf7f7344ac4ea61231";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=São%20Paulo&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl);
-
-axios.get(apiUrl).then(showTemperature);
+axios.get(apiUrl).then(displayCurrentData);
